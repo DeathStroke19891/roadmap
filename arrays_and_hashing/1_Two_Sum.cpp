29d42ae4c848ec61dcb83d976340b2296d10164d
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-08-01 07:28:59 +0530

    Minor indentation changes

--- arrays_and_hashing/1_Two_Sum.cpp
+++ arrays_and_hashing/1_Two_Sum.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            map[nums[i]] = i;
-        }
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (map.count(complement) && map[complement] != i) {
-                return {i, map[complement]};
-            }
-        }
-        return {};
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> map;
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+      map[nums[i]] = i;
     }
+    for (int i = 0; i < n; i++) {
+      int complement = target - nums[i];
+      if (map.count(complement) && map[complement] != i) {
+        return {i, map[complement]};
+      }
+    }
+    return {};
+  }
 };
+
 int main(){
   Solution sol;
   vector<int> nums = {1, 2, 3, 5};
